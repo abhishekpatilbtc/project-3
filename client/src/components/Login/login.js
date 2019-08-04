7b9e0adca093130ee3745eea309a16018b469d35
Alla Akiyeva <alla.akiyeva@gmail.com>
2019-08-04 17:48:03 -0400

    signup page onclick and axios.post

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-function Login () {
+class Login extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleClick(event) {
+    const apiBaseUrl = "http://localhost:3000/api";
+    const payload = {
+      "email": this.state.username,
+      "password": this.state.password
+    }
+    axios.post(apiBaseUrl + '/login', payload)
+    .then(function (res) {
+      console.log(res);
+      if (res.data.code === 200) {
+        console.log("Login successful");
+        const uploadScreen = [];
+        uploadScreen.push(<uploadScreen appContext={this.props.appContext}/>)
+        this.props.appContext.setState({loginPage: [], uploadScreen: uploadScreen})
+      }
+      else if (res.data.code === 204) {
+        console.log("Username and password do not match");
+        alert("Username and password do not match")
+      }
+      else {
+        console.log("Username does not exist");
+        alert("Username does not exist");
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+  }
+
+
+
+  
+
+  render () {
     return (
         <div className="row">
         <div className="col s12 m4 offset-m4">
@@ -12,12 +55,14 @@ function Login () {
 
               <div className="form-field">
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" />
+                <input type="text" id="username" 
+                  onChange = {( event, newValue ) => this.setState ({ username: newValue})}/>
               </div><br />
 
               <div className="form-field">
                 <label htmlFor="username">Password</label>
-                <input type="password" id="password" />
+                <input type="password" id="password" 
+                  onChange = {( event, newValue ) => this.setState ({ password: newValue })}/>
               </div><br />
 
               <div className="form-field">
@@ -38,6 +83,10 @@ function Login () {
         </div>
       </div>
     );
+  }
 }
+
+
+
 
 export default Login;
--- client/src/components/Signup/signup.js
+++ client/src/components/Signup/signup.js
@@ -1,63 +1,124 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+// import Login from "../Login/login";
 
-function Signup () {
-  return (
-    <div className="row">
-      <div className="col s12 m4 offset-m4">
-        <div className="card">
-          <div className="card-action teal lighten-1 white-text">
-            <h3>Sign Up</h3>
-          </div>
-          <div className="card-content">
-            <div className="row">
-              <div className="col s6">
-                <div className="form-field">
-                <label htmlFor="firstname">First Name</label>
-                <input type="text" id="firstname" />
-                </div><br />
-              </div>
-              <div className="col s6">
-                <div className="form-field">
-                <label htmlFor="lastname">Last Name</label>
-                <input type="text" id="lastname" />
-                </div><br />
-              </div>
+class Signup extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      first_name: '',
+      last_name: '',
+      email: '',
+      username: '',
+      password: ''
+    }
+  }
+
+  handleClick(event) {
+    const apiBaseUrl = "http://localhost:3001/api";
+    const payload = {
+      "first name": this.state.first_name,
+      "last name": this.state.last_name,
+      "email": this.state.email,
+      "username": this.state.username,
+      "password": this.state.password
+    }
+    console.log(payload);
+    axios.post(apiBaseUrl + '/register', payload)
+    .then(function (res) {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+      console.log(res);
+      // if (res.data.code === 200) {
+      //   console.log ("Registration successfull");
+      //   const loginscreen = [];
+      //   loginscreen.push(<Login parentContext = {this}/>);
+      //   const loginmessage = "Not registered yet. Go to registration";
+      //   this.props.parentContext.setState ({ 
+      //     loginscreen: loginscreen,
+      //     loginmessage: loginmessage,
+      //     buttonLabel: "Register",
+      //     isLogged: true
+      //   });
+      // }
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+  }
+
+  render () {
+    return (
+      <div className="row">
+        <div className="col s12 m4 offset-m4">
+          <div className="card">
+            <div className="card-action teal lighten-1 white-text">
+              <h3>Sign Up</h3>
             </div>
-
-            <div className="form-field">
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" />
-            </div><br />
-
-            <div className="form-field">
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" />
-            </div><br />
-
-            <div className="form-field">
-              <label htmlFor="username">Password</label>
-              <input type="password" id="password" />
-            </div><br />
-
-            <div className="form-field">
-              <label>
-                <input type="checkbox"/>
-                <span>Remember me</span>
-              </label>
-            </div><br />
-
-            <div className="form-field">
-              <button className="btn-large waves-effect waves-dark" style={{width: '100%'}}>Submit</button>
-            </div><br />
-
-            <span>Already have an account? <a href="/login">Log in</a></span>
-
+            <div className="card-content">
+              <div className="row">
+                <div className="col s6">
+                  <div className="form-field">
+                  <label htmlFor="firstname">First Name</label>
+                  <input type="text" id="firstname" 
+                    // onChange={this.handleInputChange}
+                    onChange = {(event) => this.setState ({ first_name: event.target.value})}
+                    />
+                  </div><br />
+                </div>
+                <div className="col s6">
+                  <div className="form-field">
+                  <label htmlFor="lastname">Last Name</label>
+                  <input type="text" id="lastname" 
+                    onChange={ (event) => this.setState ({last_name: event.target.value})}/>
+                  </div><br />
+                </div>
+              </div>
+  
+              <div className="form-field">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" 
+                  onChange = {( event ) => this.setState ({ email: event.target.value})}/>
+              </div><br />
+  
+              <div className="form-field">
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" 
+                  onChange = {( event ) => this.setState ({ username: event.target.value})}/>
+              </div><br />
+  
+              <div className="form-field">
+                <label htmlFor="username">Password</label>
+                <input type="password" id="password" 
+                  onChange = {( event ) => this.setState ({ password: event.target.value})}/>
+              </div><br />
+  
+              <div className="form-field">
+                <label>
+                  <input type="checkbox"/>
+                  <span>Remember me</span>
+                </label>
+              </div><br />
+  
+              <div className="form-field">
+                <button className="btn-large waves-effect waves-dark" 
+                  style={{width: '100%'}}
+                  onClick = {(event) => this.handleClick(event)}>
+                  Submit
+                </button>
+              </div><br />
+  
+              <span>Already have an account? <a href="/login">Log in</a></span>
+  
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    
-  );
+    );
+  }
+
+
 }
+
+
 
 export default Signup;
