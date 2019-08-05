84fd58ef1d40a82eb406df5a1fac229ff04a4cea
Abhishek Patil <AbhishekPatil@gatech.edu>
2019-08-05 00:52:46 -0400

    restructuring routes

--- routes/api/authentication.js
+++
@@ -1,7 +0,0 @@
-// const router = require("express").Router();
-// const authenticationController = require('../../controllers/authenticationController')
-// router.use("/",authenticationController)
-// module.exports = router;
-
-
-
