1b3dde44639798f2e9317d6eea73844529f6de9e
Abhishek Patil <AbhishekPatil@gatech.edu>
2019-08-07 19:25:59 -0400

    added friend request API

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
+const User = require('../../models/user');
 
 
 router.route("/")
@@ -8,7 +9,19 @@ router.route("/")
 router.route("/:id")
     .get(userController.findById)
 
-
+router.post('/:id/:friendid', (req, res) => {
+    const id = req.params.id
+    const friendid = req.params.friendid
+    console.log("user is:" + id)
+    console.log("friend is:" + friendid)
+    User.findById(id)
+        .then(user => {
+            user.friendsList.push(friendid);
+            user.save().then(res.send({message: 'Friend added successfully!'}))
+        }).catch((err) => {
+            console.log(err)
+        });
+});
 
 
 module.exports = router;
