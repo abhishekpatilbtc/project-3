fb6b96d2861a72fd05b8fb650fcb89771167ddf3
admin <47126450+abhishekpatilbtc@users.noreply.github.com>
2019-08-08 12:27:17 -0400

    autosearch added to transactions page

--- client/src/components/Request_Send/request_send.js
+++ client/src/components/Request_Send/request_send.js
@@ -1,56 +1,141 @@
 import React, { Component } from "react";
-import axios from "axios";
+import Autosuggest from 'react-autosuggest';
+import API from "../../utils/API";
+const id = localStorage.getItem('User');
+var friendsArray = [];
+
+
+API.getFriends(id)
+  .then(res => {
+    for (var i = 0; i < res.data.user.friendsList.length; i++) {
+      // friendsArray.push(res.data.user.friendsList[i].username)
+      friendsArray.push(res.data.user.friendsList[i])
+    }
+  }).catch(err => console.log(err));
+
+console.log("---------")
+console.log(friendsArray)
+console.log("---------")
+
+//  how to calculate suggestions for any given input value.
+const getSuggestions = value => {
+
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+  return inputLength === 0 ? [] : friendsArray.filter(lang =>
+    lang.username.toLowerCase().slice(0, inputLength) === inputValue
+  );
+};
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.username;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <div>
+    {suggestion.username}
+  </div>
+);
+
 
 class Request extends Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
     this.state = {
-      user: '',
-      receiver: '',
-      sender: '',
-      amount: ''
+      value: '',
+      suggestions: []
     }
   }
 
-  handleClick = event => {
-    const payload = {
-      user: '',
-      "receiver": this.state.receiver,
-      "sender": this.state.sender,
-      "amount": this.state.amount
-    }
-    axios.post('/api/transactions', payload)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
+  //////////
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
     });
-  }
+  };
 
-  render () {
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+  /////////
+
+
+
+
+
+
+  // handleClick = event => {
+  //   const payload = {
+  //     user: '',
+  //     "receiver": this.state.receiver,
+  //     "sender": this.state.sender,
+  //     "amount": this.state.amount
+  //   }
+  //   axios.post('/api/transactions', payload)
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }
+
+  render() {
+
+    const { value, suggestions } = this.state;
+
+    // Autosuggest will pass through all these props to the input.
+    const inputProps = {
+      placeholder: 'Search a friend',
+      value,
+      onChange: this.onChange
+    };
+
+
+
     return (
       <div className="row">
         <div className="col s12 m8 offset-m2">
           <div className="card">
             <div className="card-content">
-              <div className="card-action teal lighten-1 white-text" style={{paddingTop: '0', paddingBottom: '5px'}}>
+              <div className="card-action teal lighten-1 white-text" style={{ paddingTop: '0', paddingBottom: '5px' }}>
                 <h4>New Transaction</h4>
               </div><br />
+              <div className="form-field">
+                <label htmlFor="sum">Friend</label>
+                <Autosuggest
+                  suggestions={suggestions}
+                  onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                  onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                  getSuggestionValue={getSuggestionValue}
+                  renderSuggestion={renderSuggestion}
+                  inputProps={inputProps}
+                />
+              </div><br />
 
-              <div className="form-field" style={{borderBottom: '1px solid #9e9e9e'}}>
+              {/* <div className="form-field" style={{ borderBottom: '1px solid #9e9e9e' }}>
                 <label htmlFor="select">Choose a friend</label>
 
-                <select style={{display: 'unset'}}
-                  onChange = {( event ) => this.setState ({ receiver: event.target.textContent })}>
-                  <option defaultValue />
-                  <option value={1}>Aunt Meredith</option>
-                  <option value={2}>Bob from work</option>
-                  <option value={3}>Uncle Anirudh</option>
-                  <option value={4}>Becky</option>
+                <select style={{ display: 'unset' }}
+                  onChange={(event) => this.setState({ receiver: event.target.textContent })}>
+                
                 </select>
                 
-              </div><br />
+
+              </div><br /> */}
 
               <div className="form-field">
                 <label htmlFor="sum">Amount</label>
@@ -58,7 +143,7 @@ class Request extends Component {
               </div><br />
 
               <button className="waves-effect waves-light btn"
-                onClick = {(event) => this.handleClick(event)}>Log a transaction
+                onClick={(event) => this.handleClick(event)}>Log a transaction
               </button>
 
             </div>
