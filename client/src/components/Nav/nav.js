50950fccf8fe466a9db45eead0f188148c86e0f5
Abhishek Patil <AbhishekPatil@gatech.edu>
2019-08-07 22:56:57 -0400

    linked Home page to user specific

--- client/src/components/Nav/nav.js
+++ client/src/components/Nav/nav.js
@@ -24,18 +24,18 @@ class Nav extends Component {
         return (
             <nav>
                 <div className="nav-wrapper" style={{width: '70%', margin: 'auto', backgroundColor: 'white', boxShadow: 'none'}}>
-                <a href='/' className="brand-logo">Goodwill Lending</a>
+                <a href='/' className="brand-logo">Equity</a>
         {localStorage.getItem('token') ?
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li> <a href='/user' id="login">Home</a></li>
+                <li> <a href={'/user/' + localStorage.getItem('User')} id="login">Home</a></li>
                 <li> <a href='/friends' id="login">Friends</a></li>
                 <li> <a href='/userlist' id="login">Search</a></li>
-                <li> <a href='/' id="login" onClick={this.handleLogoutClick}>Log out</a></li>
+                <li> <a href='/' id="login" onClick={this.handleLogoutClick}>Logout</a></li>
             </ul>
         :
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li> <a href='/login' id="login">Log in</a></li>
-                <a className="waves-effect waves-light btn" id="signup" href="/signup">Sign up</a>
+                <a className="waves-effect waves-light btn" id="signup" href="/signup">Sign Up</a>
             </ul>}
 
                 </div>
