2155f41d6b84bbff5814de558c57a8f7a5080d6a
Abhishek Patil <AbhishekPatil@gatech.edu>
2019-08-09 14:34:59 -0400

    navbar changes

--- client/src/components/Nav/nav.js
+++ client/src/components/Nav/nav.js
@@ -8,7 +8,7 @@ class Nav extends Component {
         super(props);
         this.handleLoginClick = this.handleLoginClick.bind(this);
         this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.state = {redirectTo: null}
+        this.state = {}
     }
 
     handleLoginClick() {
@@ -35,7 +35,7 @@ class Nav extends Component {
             </ul>
         :
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li> <a href="/login" id="login" onClick={this.setState({redirectTo:"/login"})} >Log in</a></li>
+                <li> <a href="/login" id="login" >Log in</a></li>
                 <a className="waves-effect waves-light btn" id="signup" href="/signup">Sign Up</a>
             </ul>}
 
